bfcf750041ada0c4303a1f8e935d92e7b3fbd208
shyamalmahata720 <59358872+shyamalmahata720@users.noreply.github.com>
2022-06-08 17:22:55 +0530

    update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '7f2fca89-2516-40e6-9732-8acbf5d1945d';
 export const MODAL_CLOSE_SEC = 2.5;
